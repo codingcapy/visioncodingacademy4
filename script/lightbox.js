3fa6db8f917460f4736f29a3e6210765e9311b3c
Paul K <spkim0921@gmail.com>
2024-05-16 11:00:16 -0700

    46th commit
    implementing lightbox effect on about page team and testimonials page, created lighbox.js and lightbox classes styling in style.css file

---
+++ script/lightbox.js
@@ -0,0 +1,56 @@
+const images = document.querySelectorAll('.image');
+
+images.forEach((image, index) => {
+  image.addEventListener('click', () => {
+    const lightbox = document.createElement('div');
+    lightbox.classList.add('lightbox');
+
+    const fullSizeImage = document.createElement('img');
+    fullSizeImage.src = image.src;
+    fullSizeImage.classList.add('lightbox-image');
+    lightbox.appendChild(fullSizeImage);
+
+    const leftArrow = document.createElement('div');
+    leftArrow.classList.add('arrow', 'left-arrow');
+    leftArrow.innerHTML = '&lt;';
+    lightbox.appendChild(leftArrow);
+
+    const rightArrow = document.createElement('div');
+    rightArrow.classList.add('arrow', 'right-arrow');
+    rightArrow.innerHTML = '&gt;';
+    lightbox.appendChild(rightArrow);
+
+    let currentIndex = index;
+
+    function showImage(index) {
+      fullSizeImage.src = images[index].src;
+    }
+
+    leftArrow.addEventListener('click', (event) => {
+      event.stopPropagation();
+      currentIndex = (currentIndex - 1 + images.length) % images.length;
+      showImage(currentIndex);
+    });
+
+    rightArrow.addEventListener('click', (event) => {
+      event.stopPropagation();
+      currentIndex = (currentIndex + 1) % images.length;
+      showImage(currentIndex);
+    });
+
+    lightbox.addEventListener('click', (event) => {
+      if (event.target === lightbox) {
+        lightbox.classList.add('fade-out');
+        setTimeout(() => {
+          document.body.removeChild(lightbox);
+        }, 500);
+      }
+    });
+
+    document.body.appendChild(lightbox);
+
+    setTimeout(() => {
+      lightbox.classList.add('fade-in');
+    }, 50);
+  });
+});
